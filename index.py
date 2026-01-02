f03f5029e4ef0d0724b6f2a1f58226ae8018ac23
juliteGarden <111869677+gerAoMidori@users.noreply.github.com>
2026-01-02 17:14:41 +0100

    Vercel test

--- index.py
+++ index.py
@@ -10,3 +10,30 @@ templates = Jinja2Templates("templates")
 @app.get("/")
 def home():
     return "Welcome"
+    
+@app.get("/check/{password}")
+async def check(password : str):
+    result = PassWordManager().check_strength(password=password)
+    return { "password" : result }
+
+
+@app.get("/create/")
+async def create(request: Request,
+                 length: Optional[int] = None,
+                 upper: Optional[bool] = Query(None),
+                 digits: Optional[bool] = Query(None),
+                 special: Optional[bool] = Query(None)):
+    number_of_types = 1
+    length = length if length is not None else 12
+    if length < 2:
+        return {"messsage" : "Length too short"} 
+    #Upper 
+    upper = upper if upper is not None else False
+    #Digits
+    digits = digits if digits is not None else False
+    #Special
+    special = special if special is not None else False
+    print(upper, digits, special)
+    number_of_types = sum([1, upper, digits, special])
+    password = PassWordManager().create(length, number_of_types, upper, digits, special)
+    return {"password" : password }
