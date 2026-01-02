67c4e2c2e06efc4f90cff15f6e97b4fa2fa2847b
juliteGarden <111869677+gerAoMidori@users.noreply.github.com>
2026-01-02 17:05:12 +0100

    Build Test

--- index.py
+++ index.py
@@ -1,11 +1,7 @@
-from passwordManager import PassWordManager
-from typing import Optional
-from fastapi import FastAPI, Request, Query
 
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 
 app = FastAPI()
-templates = Jinja2Templates("templates")
 
 @app.get("/")
 def home():
